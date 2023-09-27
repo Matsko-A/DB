5ae1e4fb3d19b0f758da6ce58ea293ed65c19212
Matsko-A <antonmacko2002@gmail.com>
2023-09-27 15:35:45 +0300

    Проиндексированны все файл включая новый gitignore

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -1,77 +1,48 @@
 ﻿#include <iostream>
 #include <stdexcept>
 #include <random>
+#include <string>
 using namespace std;
 
 /**
- * @brief 
- * @param arr 
- * @param size 
+ * @brief Функция заполнения массива arr
+ * @param arr массив
+ * @param size размер массива
 */
-void arrayInput(int* arr, const int size);
+void arrayInput(int* arr, size_t size);
 
 /**
- * @brief 
- * @param arr 
- * @param size 
- * @return 
+ * @brief Функция сложения всех элементов массива
+ * @param arr массив
+ * @param size размер массива
+ * @return Возвращает сумму всех элементов массива
 */
-int arraySum(int* arr, const int size);
+int arraySum(int* arr, size_t size);
 
 /**
- * @brief 
- * @param arr 
- * @param size 
+ * @brief Функция вывода элементов массива в строку с помощью to_string
+ * @param arr массив
+ * @param size размер массива
 */
-void arrayPrint(int* arr, const int size);
+void arrayPrint(int* arr, size_t size);
 
 /**
- * @brief 
- * @param size  
+ * @brief Функция ввода размера массива 
+ * @param message строка ввода массива
+ * @return Возвращает размер массива равный 0 или больше
 */
-void errorSize(const int size);
-
-void arrayInput(int* arr, const int size) {
-	random_device rd;
-	mt19937 gen(rd());
-
-	uniform_int_distribution<> uniformIntDistribution(0, 100);
-
-	for (int i = 0; i < size; i++) {
-		arr[i] = uniformIntDistribution(gen);
-	}
-}
-
-int arraySum(int* arr, const int size) {
-	int count = 0;
-	for (int i = 0; i < size; i++) {
-		count += arr[i];
-	}
-	
-	return count;
-}
-
-void arrayPrint(int* arr, const int size) {
-	for (int i = 0; i < size; i++) {
-		cout << "[" << arr[i] << "] ";
-	}
-}
-
-void errorSize(const int size) {
-	if (size <= 0) throw invalid_argument("Error size array");
-}
+size_t get_size(const string& message);
 
 int main() {
 	setlocale(LC_ALL, "rus");
 
-	int size = 0;
-	cout << "Введите размер массива: ";
-	cin >> size;
+	string message = "Введите размер массива: ";
+	size_t size = 0;
 
 	try {
-		errorSize(size);
+		size = get_size(message);
 	}
-	catch (invalid_argument& e) {
+	catch (out_of_range& e) {
 		cout << e.what() << endl;
 		return -1;
 	}
@@ -86,7 +57,48 @@ int main() {
 
 	cout << "Сумма элементов массива: " << arraySum(arr, size) << endl;
 
-	delete[] arr;
+	if (arr != nullptr) {
+		delete[] arr;
+		arr = nullptr;
+	}
 
 	return 0;
+}
+
+void arrayInput(int* arr, size_t size) {
+	random_device rd;
+	mt19937 gen(rd());
+
+	uniform_int_distribution<> uniformIntDistribution(0, 100);
+
+	for (int i = 0; i < size; i++) {
+		arr[i] = uniformIntDistribution(gen);
+	}
+}
+
+int arraySum(int* arr, size_t size) {
+	int count = 0;
+	for (int i = 0; i < size; i++) {
+		count += arr[i];
+	}
+
+	return count;
+}
+
+void arrayPrint(int* arr, size_t size) {
+	for (int i = 0; i < size; i++) {
+		cout << "[" << to_string(arr[i]) << "] ";
+	}
+}
+
+size_t get_size(const string& message) {
+	int  size = -1;
+	cout << message;
+	cin >> size;
+
+	if (size < 0) {
+		throw out_of_range("Неправильный размер. Значение должно быть больше или равно нулю.");
+	}
+
+	return size;
 }
